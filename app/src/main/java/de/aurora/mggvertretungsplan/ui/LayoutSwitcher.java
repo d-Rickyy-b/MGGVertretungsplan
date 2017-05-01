b45b5c0bd9bf3baa0ee56a0737a3d3f1627111be
Rico <r.jambor@yahoo.de>
2017-05-01 04:56:39 +0200

    FEATURE: Add method to get the ID for a theme

--- app/src/main/java/de/aurora/mggvertretungsplan/ui/LayoutSwitcher.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/ui/LayoutSwitcher.java
@@ -5,6 +5,9 @@ import android.support.annotation.StyleRes;
 import de.aurora.mggvertretungsplan.R;
 
 public class LayoutSwitcher {
+    private static final int bluePrimary = 0xFF64B5F6;
+    private static final int pinkPrimary = 0xFFD81B60;
+    private static final int stdPrimary = 0xFF757575;
 
     @StyleRes
     public static int getTheme(int themeID) {
@@ -17,6 +20,19 @@ public class LayoutSwitcher {
                 return R.style.PinkTheme;
             default:
                 return R.style.AppTheme;
+        }
+    }
+
+    public static int getID(int color){
+        switch (color) {
+            case stdPrimary:
+                return 0;
+            case bluePrimary:
+                return 1;
+            case pinkPrimary:
+                return 2;
+            default:
+                return 0;
         }
     }
 }
