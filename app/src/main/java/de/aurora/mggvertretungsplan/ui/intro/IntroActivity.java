916227819f8e94ea87197bc3e872066ce3ddf557
Rico <r.jambor@yahoo.de>
2017-05-04 14:57:46 +0200

    TASK: Remove unused import

--- app/src/main/java/de/aurora/mggvertretungsplan/ui/intro/IntroActivity.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/ui/intro/IntroActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.ColorInt;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 
 import com.github.paolorotolo.appintro.AppIntro;
