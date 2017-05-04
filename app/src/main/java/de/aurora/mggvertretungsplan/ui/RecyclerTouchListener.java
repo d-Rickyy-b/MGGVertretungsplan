0434e1079f6681657ce03edf0dd5012af021dccb
Rico <r.jambor@yahoo.de>
2017-05-04 15:40:25 +0200

    TASK: Remove deprecated methods

--- app/src/main/java/de/aurora/mggvertretungsplan/ui/RecyclerTouchListener.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/ui/RecyclerTouchListener.java
@@ -27,7 +27,7 @@ class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
             public void onLongPress(MotionEvent e) {
                 View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
                 if (child != null && clickListener != null) {
-                    clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
                 }
             }
         });
@@ -38,7 +38,7 @@ class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
     public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
         View child = rv.findChildViewUnder(e.getX(), e.getY());
         if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
-            clickListener.onClick(child, rv.getChildPosition(child));
+            clickListener.onClick(child, rv.getChildAdapterPosition(child));
         }
         return false;
     }
