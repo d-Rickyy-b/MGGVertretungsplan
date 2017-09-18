3ca12dcc3b36c94973c8da9a646c2d2267c4181c
Rico <r.jambor@yahoo.de>
2017-09-18 19:51:53 +0200

    TASK: Optimize imports

--- app/src/main/java/de/aurora/mggvertretungsplan/MainActivity.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/MainActivity.java
@@ -2,7 +2,6 @@ package de.aurora.mggvertretungsplan;
 
 import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
