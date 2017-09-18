3194564b335303cb16c523e2c876a04dcdc46338
Rico <r.jambor@yahoo.de>
2017-09-18 18:45:44 +0200

    TASK: Remove unused import

--- app/src/main/java/de/aurora/mggvertretungsplan/hilfsMethoden.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/hilfsMethoden.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
