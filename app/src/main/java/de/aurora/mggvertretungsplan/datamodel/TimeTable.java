24f85e37a57641fb7b6e30409346e9bc528ec18f
Rico <r.jambor@yahoo.de>
2017-09-25 00:55:34 +0200

    TASK: (WIP) Add method to get total diffs
    between two time tables

--- app/src/main/java/de/aurora/mggvertretungsplan/datamodel/TimeTable.java
+++ app/src/main/java/de/aurora/mggvertretungsplan/datamodel/TimeTable.java
@@ -1,6 +1,9 @@
 package de.aurora.mggvertretungsplan.datamodel;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Rico on 19.09.2017.
@@ -47,6 +50,36 @@ public class TimeTable {
         }
 
         return cancellations;
+    }
+
+    public int getTotalDifferences(TimeTable savedTimeTable) {
+        int differences = 0;
+        Date currentDate = new Date();
+        ArrayList<TimeTableDay> savedDays = savedTimeTable.getAllDays();
+
+        for (TimeTableDay ttd : timeTableDays) {
+            boolean sameDaySaved = false;
+            int sixteenHrsInSecs = 60 * 60 * 16;
+            if (currentDate.getTime() > ttd.getDate().getTime() + sixteenHrsInSecs)
+                continue;
+
+            for (TimeTableDay saved_ttd : savedDays) {
+
+                if (ttd.isSameDay(saved_ttd)) {
+                    Log.d("BackgroundService", String.format("Dates are the same - %s | %s", ttd.getDateString(), saved_ttd.getDateString()));
+                    Log.d("BackgroundService", String.format("%s", ttd.toString()));
+                    Log.d("BackgroundService", String.format("%s", saved_ttd.toString()));
+                    differences += ttd.getDifferences(saved_ttd);
+                    sameDaySaved = true;
+                    break;
+                }
+            }
+
+            if (!sameDaySaved)
+                differences += ttd.getCancellations();
+        }
+
+        return differences;
     }
 
     @Override
