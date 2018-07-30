fb5fea4cd5c4dd4688cec8e2fdd4b0c182eb868f
Rico <r.jambor@yahoo.de>
2018-07-30 22:38:11 +0200

    TASK: Implement ConnectionManager to be able to check for connectivity

---
+++ app/src/main/java/de/aurora/mggvertretungsplan/networking/ConnectionManager.java
@@ -0,0 +1,23 @@
+package de.aurora.mggvertretungsplan.networking;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectionManager {
+    private final static String TAG = "ConnectionManager";
+
+    public static boolean isConnectionActive(Context context) {
+        final ConnectivityManager conMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (null == conMgr) {
+            return false;
+        }
+
+        final NetworkInfo activeNetwork = conMgr.getActiveNetworkInfo();
+
+        return null != activeNetwork && activeNetwork.isConnected();
+
+    }
+
+}
