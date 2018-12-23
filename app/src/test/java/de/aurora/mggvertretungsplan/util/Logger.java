5d2f7a4ccaf14aa312ddfc2d06a259567c6ed71a
Rico <d-Rickyy-b@users.noreply.github.com>
2018-12-23 23:01:38 +0100

    FIX: Implement mock Logger class for tests

---
+++ app/src/test/java/de/aurora/mggvertretungsplan/util/Logger.java
@@ -0,0 +1,39 @@
+package de.aurora.mggvertretungsplan.util;
+
+public class Logger {
+
+    public Logger() {
+    }
+
+    public static void sendLogs() {
+
+    }
+
+    public static void deleteLogs() {
+    }
+
+    public static void d(final String tag, final String message) {
+
+    }
+
+    public static void v(final String tag, final String message) {
+
+    }
+
+    public static void i(final String tag, final String message) {
+
+    }
+
+    public static void w(final String tag, final String message) {
+
+    }
+
+    public static void e(final String tag, final String message) {
+
+    }
+
+    public static void wtf(final String tag, final String message) {
+
+    }
+}
+
